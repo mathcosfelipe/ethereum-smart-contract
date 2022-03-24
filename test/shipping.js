58e8c6e329e8167e5c9278d20095b84b7f5ba8e5
Marcos Felipe <marcosfbdcarvalho@gmail.com>
2022-03-24 14:16:02 -0300

    Update shipping.js

--- test/shipping.js
+++ test/shipping.js
@@ -1,13 +1,40 @@
-const Shipping = artifacts.require("Shipping");
+const ShippingStatus= artifacts.require("Shipping");
+contract('Shipping', () => {
 
-/*
- * uncomment accounts to access the test accounts made available by the
- * Ethereum client
- * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
- */
-contract("Shipping", function (/* accounts */) {
-  it("should assert true", async function () {
-    await Shipping.deployed();
-    return assert.isTrue(true);
+  it("should return the status Pending", async ()=> {
+    // Instance of our deployed contract
+    const instance = await ShippingStatus.deployed();
+    // Checking the initial status in our contract
+    const status = await instance.Status();
+    // Checking if the status is initially Pending as set in the constructor
+    assert.equal(status, "Pending");
+  });
+it("should return the status Shipped", async ()=> {
+// Instance of our deployed contract
+    const instance = await ShippingStatus.deployed();
+
+    // Calling the Shipped() function
+    await instance.Shipped();
+
+    // Checking the initial status in our contract
+    const status = await instance.Status();
+
+    // Checking if the status is Shipped
+    assert.equal(status, "Shipped");
+  });
+
+    it("should return the status Delivered", async ()=> {
+
+    // Instance of our deployed contract
+    const instance = await ShippingStatus.deployed();
+
+    // Calling the Shipped() function
+    await instance.Delivered();
+
+    // Checking the initial status in our contract
+    const status = await instance.Status();
+
+    // Checking if the status is Delivered
+    assert.equal(status, "Delivered");
   });
 });
